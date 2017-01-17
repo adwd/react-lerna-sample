bfa2f010262d37dcbc6e942313132c2d1898fee0
西田 雅博 <masahiro.nishida@bizreach.co.jp>
2017-01-17 19:33:28 +0900

    WIP test does not run

--- packages/react-app/src/App.test.js
+++ packages/react-app/src/App.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+import { SampleComponent } from 'my-react-components';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('renders sample component', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.contains(<SampleComponent />)).toEqual(true);
 });
