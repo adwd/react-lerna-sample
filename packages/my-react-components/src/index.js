459d1ef786b529c33bc8552f2008e8b0581481b9
西田 雅博 <masahiro.nishida@bizreach.co.jp>
2017-01-17 19:02:26 +0900

    add component library

---
+++ packages/my-react-components/src/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class SampleComponent extends Component {
+  render() {
+    return (
+      <div>
+        <p>this is sample component</p>
+      </div>
+    );
+  }
+}
